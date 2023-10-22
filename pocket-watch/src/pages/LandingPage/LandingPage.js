54ce9467d6be73e09c5bed6c15f589163ed4f1ab
KevSalad <112964203+KevSalad@users.noreply.github.com>
2023-10-21 22:45:43 -0500

    Initial Submit

--- pocket-watch/src/pages/LandingPage/LandingPage.js
+++ pocket-watch/src/pages/LandingPage/LandingPage.js
@@ -2,17 +2,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
-function LandingPage(){
+// Navbar Component
+function Navbar() {
+    return (
+    
+    <nav className="navbar">
+        <h1 className='logo-text'>Pocket Watchers</h1> 
+        <ul className='left-side'>
+            <li>
+                <Link to="/coruses">Courses</Link>
+            </li>
+            <li>
+                <Link to="/expenses">Expenses</Link>
+            </li>
+            <li>
+                <Link to="/calculator">Calculator</Link>
+            </li>
+        </ul>
+        
+        <div className='right-side'>
+            <button className='contact0button'>
+                <Link to="/signup">Contact Us</Link>
+            </button>
+            <button className='sign-button'>
+                <Link to="/signup">Sign Up</Link>
+            </button>
+        </div>
+      </nav>
+    );
+}
+
+function LandingPage() {
     return (
         <div>
-            <h1>Sample Text for Landing Page</h1>
-            <nav>
+            <Navbar />
+            
+            {/* <nav>
                 <ul>
                     <li>
                         <Link to="/signUp">Sign Up</Link>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
         </div>
     );
 }
