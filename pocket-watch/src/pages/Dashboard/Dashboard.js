325ea74d32b8a8fb98a1763300e97f3787dc1fe6
KevSalad <112964203+KevSalad@users.noreply.github.com>
2023-11-09 19:50:12 -0600

    Frontend almost complete

--- pocket-watch/src/pages/Dashboard/Dashboard.js
+++ pocket-watch/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
+import Select from 'react-select'
+
+
 
 // Navbar Component
 function Navbar() {
@@ -31,12 +34,21 @@ function Navbar() {
 }
 
 function DashboardSum() {
+
+    const options = [
+        { value: 'Current', label: 'Current' },
+        { value: '1 Month Ago', label: '1 Month Ago' },
+        { value: '2 Months Ago', label: '2 Months Ago' },
+        { value: '3 Months Ago', label: '3 Months Ago' }
+      ]
+    
     return (
         <div class="background">
             <div class="b-div top-div">
                 <h1 class="user">Hello, User</h1>
             </div>
             <div class="b-div bot-div"></div>
+
             <div class="dashboard">
                 <form class="grid-container">
                     <div class="grid-item grid-item-1">
@@ -54,13 +66,91 @@ function DashboardSum() {
                     </div>
 
                     <div class="grid-item grid-item-2">
-                        
+                        {/* <img className='grid-background' src='/public/images/Courses-Picture.png' alt='Course'/>
+                         */}
+                        <div className='risk-div'>
+                            <p className='risk'> Risk Management </p>
+                            <p className='small-text'> Learn more about insurance, emergency funds, and risk mitigation </p>
+                        </div>
+                        <button className='course-button'> Start Course </button>
                     </div>
+
                     <div class="grid-item grid-item-3">
+                        <div className='Transactions'> 
 
+                            <div className='t-header'>
+                                <p className='t-text'> Latest Transactions </p>
+                                <button class="button-arrow">
+                                    <img src="/images/Arrow.svg" alt="SVG5" class="svg-arrow" />
+                                </button>
+                            </div>
+        
+                            <div className='transaction-history'>
+                                <p className='date'> 10-22 </p>
+                                <p className='company'> Gongcha </p>
+                                <p className='category'> Food </p>
+                                <p className='cost'> $ -15.00</p>
+                            </div>
+                            <div className='transaction-history'>
+                                <p className='date'> 10-20 </p>
+                                <p className='company'> Amazon </p>
+                                <p className='category'> Shopping </p>
+                                <p className='cost'> $ -50.00</p>
+                            </div>
+                            <div className='transaction-history'>
+                                <p className='date'> 10-18 </p>
+                                <p className='company'> Target </p>
+                                <p className='category'> Shopping </p>
+                                <p className='cost'> $ -30.00</p>
+                            </div>
+                            <div className='transaction-history'>
+                                <p className='date'> 10-15 </p>
+                                <p className='company'> Starbucks </p>
+                                <p className='category'> Food </p>
+                                <p className='cost'> $ -10.00</p>
+                            </div>
+                            
+                            <div className='see-more-footer'>
+                                <button class="button-arrow">
+                                    <p className='see-more'> See More </p>
+                                    <img src="/images/Arrow.svg" alt="SVG6" class="arrow" />
+                                </button>
+                            </div>
+                        </div>
                     </div>
+                    
                     <div class="grid-item grid-item-4">
+                        <div className='Expenses'>
 
+                            <div className='e-header'>
+                                <p className='e-text'> Expenses </p>
+                                <button class="button-arrow">
+                                    <img src="/images/Arrow.svg" alt="SVG7" class="svg-arrow" />
+                                </button>
+                            </div>
+                            <div className='e-info'>
+                                <div className='dropdown'>
+                                    <Select options={options} 
+                                    styles={{
+                                        control: (baseStyles, state) => ({
+                                            ...baseStyles,
+                                            borderStyle: 'none', 
+                                            fontSize : '12px',
+                                        }),
+                                        menu: (baseStyles, state) => ({
+                                            ...baseStyles,
+                                            fontSize : '12px',
+                                            margin: '0px',
+                                        }),
+                                    }}
+                                    />
+                                </div>
+                                <div className='e-cost'>
+                                    <p className='small-text'> Monthly </p>
+                                    <p className='cost-text'> $ 149.67 </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
